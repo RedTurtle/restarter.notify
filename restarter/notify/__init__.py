472e1045036c9e7c6261f8f4f7257230c9c04231
Andrew Mleczko <andrew@mleczko.net>
2012-06-18 15:13:06 +0200

    fix notifications

--- restarter/notify/__init__.py
+++ restarter/notify/__init__.py
@@ -10,8 +10,7 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.add_route('notify_sms', '/notify/sms')
-    config.add_route('notify_email', '/notify/email')
+    config.add_route('notify', '/notify')
     config.add_route('fb_register_notify', '/notify/fb/register')
     config.add_route('fb_sell_notify', '/notify/fb/sell')
     config.add_route('fb_newcompany_notify', '/notify/fb/newcompany')
@@ -55,28 +54,27 @@ def fb_register_notify(request):
     return 'OK'
 
 
-@view_config(route_name='notify_email', renderer='json', request_param='email', request_method='POST')
-def email_notify(request):
-    email = request.params.get('email')
-    message = request.params.get('message')
-    if not message:
-        return {'KO': 'No message provided.'}
-
-    mailer = get_mailer(request)
-    emails.send_email.delay(mailer, message, email)
-    return 'OK'
-
-
-@view_config(route_name='notify_sms', renderer='json', request_param='phone', request_method='POST')
-def sms_notify(request):
+@view_config(route_name='notify', renderer='json', request_method='POST')
+def notify(request):
     settings = request.registry.settings
-    phone = request.params['phone']
-    message = request.params.get('message')
-    if not message:
-        return {'KO': 'No message provided.'}
-
     login = settings.get('sms.username')
     password = settings.get('sms.password')
-    sms.send_sms.delay(login, password, message, phone)
-    return 'OK'
+    email = request.params.get('email')
+    email_message = request.params.get('email_message')
+    phone = request.params['phone']
+    phone_message = request.params.get('phone_message')
 
+    if not email_message and phone_message:
+        return {'KO': 'No message provided.'}
+
+    if not email and phone:
+        return {'KO': 'No recipients.'}
+
+    if email:
+        mailer = get_mailer(request)
+        emails.send_email.delay(mailer, email_message, email)
+
+    if phone:
+        sms.send_sms.delay(login, password, phone_message, phone)
+
+    return 'OK'
